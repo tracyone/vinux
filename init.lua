54c148eeb68e72356e41453c862e1d4bcb115927
tracyone <tracyone@live.cn>
2023-11-24 21:04:19 +0800

    close #96 again
    Signed-off-by: tracyone <tracyone@live.cn>

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
---File       init.vim
+--File       init.lua
 --Brief      Entry file of neovim
 --Date       2015-11-28/22:56:20
 --Author     tracyone,tracyone@live.cn,
 --Github     https://github.com/tracyone/vinux
-vim.api.nvim_command("source vimrc")
+vim.api.nvim_command("source ".. vim.fn.fnamemodify(vim.fn.expand('<sfile>'), ':p:h').."/vimrc")
 
